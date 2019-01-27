9a559a5de9336cdfa42ecb96d1292c965a343469
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2019-01-26 22:30:56 -0500

    update court script

--- Assets/Code/CourtYardScript.cs
+++ Assets/Code/CourtYardScript.cs
@@ -15,4 +15,49 @@ public class CourtYardScript : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // Declare and initialize a new List of GameObjects called currentCollisions.
+    List<GameObject> currentSpecialItems = new List<GameObject>();
+
+    void OnTriggerEnter(Collision col)
+    {
+
+        // Add the GameObject collided with to the list.
+        if (col.gameObject.GetType() == typeof(BasicItem))
+        {
+            currentSpecialItems.Add(col.gameObject);
+            Debug.Log("You found a special item");
+            CheckForWin();
+        }
+
+        // Print the entire list to the console.
+        foreach (GameObject gObject in currentSpecialItems)
+        {
+            print(gObject.name);
+        }
+    }
+
+    void CheckForWin()
+    {
+        if (currentSpecialItems.Count == 2)
+        {
+            Debug.Log("YOU WIN!");
+        }
+    }
+
+    void OnTriggerExit(Collision col)
+    {
+
+        // Remove the GameObject collided with from the list.
+        if (col.gameObject.GetType() == typeof(BasicItem))
+        {
+            currentSpecialItems.Remove(col.gameObject);
+            Debug.Log("you removed a special item");
+        }
+    }
 }
