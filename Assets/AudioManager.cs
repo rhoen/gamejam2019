949dc78f87493f9ebc0ccb2e0b7542c02d23dcf5
David Shen <davidshen@asana.com>
2019-01-26 20:43:46 -0500

    music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+
+    public AudioMixerSnapshot FirstItemCollected;
+    public static AudioManager Instance { private set; get; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstItemCollected.TransitionTo(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
