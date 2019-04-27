2e8e68c565b8057e38b25606c791bb574dd34662
David Shen <davidshen@asana.com>
2019-04-27 14:34:46 -0400

    random basic item is special

--- Assets/Code/GameStateManager.cs
+++ Assets/Code/GameStateManager.cs
@@ -14,7 +14,33 @@ public class GameStateManager : MonoBehaviour
         int playerId = Random.value < 0.5f ? 1 : 2;
         PlayerController player = GameObject.FindGameObjectWithTag("player" + playerId).GetComponent<PlayerController>();
         player.DropThenPickUpBook();
+        SelectRandomItems();
     }
+
+    void SelectRandomItems() {
+        BasicItem[] allBasicItems = FindObjectsOfType<BasicItem>();
+        Shuffle(allBasicItems);
+        for (int i = 0; i < 5; i++) {
+            allBasicItems[i].specialItem = true;
+        }
+    }
+
+    void Shuffle(MonoBehaviour[] objects)
+	{
+		// Loops through array
+		for (int i = objects.Length-1; i > 0; i--)
+		{
+			// Randomize a number between 0 and i (so that the range decreases each time)
+			int rnd = Random.Range(0,i);
+			
+			// Save the value of the current i, otherwise it'll overright when we swap the values
+			MonoBehaviour temp = objects[i];
+			
+			// Swap the new and old values
+			objects[i] = objects[rnd];
+			objects[rnd] = temp;
+		}
+	}
 
     public void Lose() {
         AudioManager.Instance.Lose();
